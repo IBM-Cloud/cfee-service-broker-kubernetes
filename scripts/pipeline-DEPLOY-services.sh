1c5a966a4d1b253677501024fc543ed3d5e8034f
twanawebtech@gmail.com <twanawebtech@gmail.com>
2019-02-22 09:48:02 +0000

    Update pipeline-DEPLOY-services.sh to create Cloudant service

--- scripts/pipeline-DEPLOY-services.sh
+++ scripts/pipeline-DEPLOY-services.sh
@@ -4,18 +4,15 @@
 # login to IBM Cloud (public) to provision public services like Cloudant
 #
 echo Login IBM Cloud api=$CF_TARGET_URL org=$CF_ORG space=$CF_SPACE
-bx login -a "$CF_TARGET_URL" --apikey "$API_KEY" -o "$CF_ORG" -s "$CF_SPACE"
+ibmcloud login -a "$CF_TARGET_URL" --apikey "$API_KEY" -o "$CF_ORG" -s "$CF_SPACE"
 
-REGION=$(bx target | grep Region | awk '{ print $2 }')
+REGION=$(ibmcloud target | grep Region | awk '{ print $2 }')
+echo ${REGION}
 
-# bx target bx group ---
-#
 # create an instance of the cloudant service
-#
-echo "Checking for service instance 'cfee-cloudant'"
-if ! cf service cfee-cloudant; then
   echo "Creating service instance 'cfee-cloudant'"
-  bx resource service-instance-create cfee-cloudant cloudantnosqldb lite ${REGION}
-else
-  echo "cfee-cloudant already exists"
-fi
+  ibmcloud cf create-service cloudantNoSQLDB Lite cfee-cloudant
+  
+  # ibmcloud resource service-instance-create cfee-cloudant cloudantnosqldb lite ${REGION}
+
+
