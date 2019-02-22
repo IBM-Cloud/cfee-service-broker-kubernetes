2faf769ae0874a9e2588cc1c76bfc2a72c8d27f7
twanawebtech@gmail.com <twanawebtech@gmail.com>
2019-02-22 15:40:47 +0000

    Change deployment type to Kubernetes in the toolchain and remove login

--- scripts/pipeline-DEPLOY-getstartednode.sh
+++ scripts/pipeline-DEPLOY-getstartednode.sh
@@ -1,23 +1,36 @@
 #!/bin/bash
 
+#
+# login to IBM Cloud using the API key targeting a region and then target CFEE org and space.
+#
+echo "login to IBM Cloud using the API key targeting a region and then target CFEE org and space."
 # ibmcloud login --apikey "$API_KEY" -region "$REGION"
 ibmcloud login --apikey "$API_KEY" -r "us-south"
 ibmcloud target --cf-api "$CF_TARGET_URL" -o "$CF_ORG" -s "$CF_SPACE"
 
+#
 # create an alias in CFEE to the Cloudant services instance in public Cloud
-ibmcloud cfee service-alias-create cfee-cloudant --service-instance-name cfee-cloudant
+#
+ibmcloud cfee service-alias-create cfee-cloudant --service-instance-name Cloudant-ha
 
 
+#
 # deploy the GetStartedNode app
+#
 cd ./get-started-node
 ibmcloud cf push
 
+#
 # bind Cloudant to this app
+#
 ibmcloud cf bind-service GetStartedNode cfee-cloudant
 
-
+#
 # bind the sample "welcome" service to this app
+#
 ibmcloud cf bind-service GetStartedNode welcome-service
 
+#
 # restart the service to load bindings
+#
 ibmcloud cf restart GetStartedNode
--- scripts/pipeline-DEPLOY-services.sh
+++ scripts/pipeline-DEPLOY-services.sh
@@ -3,16 +3,13 @@
 #
 # login to IBM Cloud (public) to provision public services like Cloudant
 #
-echo Login IBM Cloud api=$CF_TARGET_URL org=$CF_ORG space=$CF_SPACE
-ibmcloud login -a "$CF_TARGET_URL" --apikey "$API_KEY" -o "$CF_ORG" -s "$CF_SPACE"
+# echo Login IBM Cloud api=$CF_TARGET_URL org=$CF_ORG space=$CF_SPACE
+# ibmcloud login -a "$CF_TARGET_URL" --apikey "$API_KEY" 
 
 REGION=$(ibmcloud target | grep Region | awk '{ print $2 }')
 echo ${REGION}
 
 # create an instance of the cloudant service
   echo "Creating service instance 'cfee-cloudant'"
-  ibmcloud cf create-service cloudantNoSQLDB Lite cfee-cloudant
-  
-  # ibmcloud resource service-instance-create cfee-cloudant cloudantnosqldb lite ${REGION}
-
+  ibmcloud resource service-instance-create cfee-cloudant cloudantnosqldb lite ${REGION}
 
