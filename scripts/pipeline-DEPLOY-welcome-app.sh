29e789e41b984b0d5ecf0a5c3f4cdae5f8462fa9
twanawebtech@gmail.com <twanawebtech@gmail.com>
2019-02-22 10:21:42 +0000

    Add ibmcloud before cf

--- scripts/pipeline-DEPLOY-welcome-app.sh
+++ scripts/pipeline-DEPLOY-welcome-app.sh
@@ -7,9 +7,9 @@ BROKER_STATUS=$(cf service-brokers | grep my-company-broker)
 if [[ "${BROKER_STATUS}" == "" ]]
 then
   echo "Creating service broker"
-  cf create-service-broker my-company-broker TestServiceBrokerUser TestServiceBrokerPassword http://tutorial-broker-service.default.svc.cluster.local
+  ibmcloud cf create-service-broker my-company-broker TestServiceBrokerUser TestServiceBrokerPassword http://tutorial-broker-service.default.svc.cluster.local
   echo "Enabling 'testnoderesourceservicebrokername' service"
-  cf enable-service-access testnoderesourceservicebrokername
+  ibmcloud cf enable-service-access testnoderesourceservicebrokername
 else
   echo "my-company-broker already exists"
 fi
@@ -18,9 +18,9 @@ fi
 # create an instance of the "welcome" service (must be done as part of the CFEE deployer type)
 #
 echo "Checking for service instance 'welcome-service'"
-if ! cf service welcome-service; then
+if ! ibmcloud cf service welcome-service; then
   echo "Creating service instance 'welcome-service'"
-  cf create-service testnoderesourceservicebrokername lite welcome-service
+  ibmcloud cf create-service testnoderesourceservicebrokername lite welcome-service
 else
   echo "welcome-service already exists"
 fi
@@ -28,11 +28,11 @@ fi
 #
 # update the ASG IP range to include the service broker (assumes the default public_networks ASG is set)
 #
-cf security-group public_networks | sed '6,28!d' | sed "s|172.32.0.0-192.167.255.255|172.1.0.0-192.167.255.255|" > public_networks.json
+ibmcloud cf security-group public_networks | sed '6,28!d' | sed "s|172.32.0.0-192.167.255.255|172.1.0.0-192.167.255.255|" > public_networks.json
 echo "Modified public_networks ASG to the following:"
 cat public_networks.json
 echo "Updating public_networks ASG"
-cf update-security-group public_networks ./public_networks.json
+ibmcloud cf update-security-group public_networks ./public_networks.json
 
 #
 # deploy the "welcome" service app to receive service requests from other apps
