8643ab2325a72b57f160295a570689add298b208
twanawebtech@gmail.com <twanawebtech@gmail.com>
2019-02-25 19:59:16 +0000

    Remove hardcoded region

--- scripts/pipeline-DEPLOY-getstartednode.sh
+++ scripts/pipeline-DEPLOY-getstartednode.sh
@@ -4,11 +4,13 @@
 # login to IBM Cloud using the API key targeting a region and then target CFEE org and space.
 #
 echo "login to IBM Cloud using the API key targeting a region and then target CFEE org and space."
-# ibmcloud login --apikey "$API_KEY" -region "$REGION"
-ibmcloud login --apikey "$API_KEY" -r "us-south"
-ibmcloud target --cf-api "$CF_TARGET_URL" -o "$CF_ORG" -s "$CF_SPACE"
+ibmcloud login --apikey "$API_KEY" 
 REGION=$(ibmcloud target | grep Region | awk '{ print $2 }')
-echo ${REGION}
+
+# Login again but target the region, also target the CFEE org and space.
+ibmcloud login --apikey "$API_KEY" -r ${REGION}
+ibmcloud target --cf-api "$CF_TARGET_URL" -o "$CF_ORG" -s "$CF_SPACE"
+
 
 #
 # create an alias in CFEE to the Cloudant services instance in public Cloud
