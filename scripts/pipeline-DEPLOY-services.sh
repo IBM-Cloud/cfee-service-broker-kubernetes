bea429a94c4881bcde8a39dfce83bd6a1d101abf
twanawebtech@gmail.com <twanawebtech@gmail.com>
2019-02-25 08:07:29 +0000

    Add IF condition NOT to create duplicate Cloudant service

--- scripts/pipeline-DEPLOY-services.sh
+++ scripts/pipeline-DEPLOY-services.sh
@@ -1,15 +1,20 @@
 #!/bin/bash
 
 #
-# login to IBM Cloud (public) to provision public services like Cloudant
-#
-# echo Login IBM Cloud api=$CF_TARGET_URL org=$CF_ORG space=$CF_SPACE
+# Login to IBM Cloud (public) to provision public services like Cloudant
+# -> Kubernetes deployment is used, meaning no need to login, the state inside inside toolchain Kubernetes is already logged into the account
+# -> echo Login IBM Cloud api=$CF_TARGET_URL org=$CF_ORG space=$CF_SPACE
 # ibmcloud login -a "$CF_TARGET_URL" --apikey "$API_KEY" 
 
+# Get region and store inside ${REGION}
 REGION=$(ibmcloud target | grep Region | awk '{ print $2 }')
-echo ${REGION}
 
 # create an instance of the cloudant service
-  echo "Creating service instance 'cfee-cloudant'"
-  ibmcloud resource service-instance-create cfee-cloudant cloudantnosqldb lite ${REGION}
+echo "Checking for service instance 'cfee-cloudant'"
+if ! ibmcloud resource service-instance cfee-cloudant; then
+    echo "Creating service instance 'cfee-cloudant'"  
+    ibmcloud resource service-instance-create cfee-cloudant cloudantnosqldb lite ${REGION}
+else
+    echo "cfee-cloudant already exists"
+fi
 
